bd2e93830dfbf27da630405a22f4aa8d38319db2
BenPHaley <bhaley7@terpmail.umd.edu>
2018-07-25 20:58:11 -0400

    What a string looks like when compiled

---
+++ CinAssembly/string_code.c
@@ -0,0 +1,5 @@
+// Taking a look at what a string looks like in assembly when compiled
+// Also what a pointer looks like
+void has_string () {
+	char *message = "Character array makes a string\n";
+}
